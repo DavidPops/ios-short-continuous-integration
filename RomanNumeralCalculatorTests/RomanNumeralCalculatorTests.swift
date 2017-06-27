f87a7502063c3c6cf6385613c74a6bebf389a600
David Popoola <davieedpopoola@gmail.com>
2017-06-27 02:02:44 +0100

    added two unit tests

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -2,11 +2,12 @@
 //  RomanNumeralCalculatorTests.swift
 //  RomanNumeralCalculatorTests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by David Popoola on 26/06/2017.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
+@testable import RomanNumeralCalculator // we import this, so we can access the main application bundle.
 
 class RomanNumeralCalculatorTests: XCTestCase {
     
@@ -20,9 +21,14 @@ class RomanNumeralCalculatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testToRoman() { //ßwe must not delete the test word, but we can remove the example. As usual, funcs are carmel cased
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(toRoman(1), "I")  //We use XCTAssertEqual here.
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(4), "IV")
+        
+        // Tests should only test the core unit functionality of your app, especially unit tests.
     }
     
     func testPerformanceExample() {
